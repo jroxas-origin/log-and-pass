215f408724ff1095128101a5a4d630c2ea16e57d
Justine Roxas <roxas.justine.zs@gmail.com>
2022-06-20 18:16:55 -0700

    added post handler

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send("Hello from server");
+});
+
+
 app.post("/get-appointments", (req, res) => {
   console.log(req.body);
   //app.get('/', verifyCognitoToken, verifyIDCSToken, verifyAuthenticated, (req, res) => {
