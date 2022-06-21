543fb2f88cd4f62ba771a0c2df3e5aa3d6df423a
Justine Roxas <roxas.justine.zs@gmail.com>
2022-06-21 10:40:02 -0700

    split rest and soap endpoints

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
 
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+
+app.use(bodyParser.json({ verify: rawBodySaver }));
+app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
+app.use(bodyParser.raw({ verify: rawBodySaver, type: '*/*' }));
+
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.status(200).send({ message: 'Hello from server!', body: req.body });
+app.post("/rest", (req, res) => {
+
+  try {
+
+    res.set('Content-Type', 'application/json');
+    res.status(200).send(JSON.parse(req.rawBody));
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.post("/soap", (req, res) => {
+
+  try {
+
+    res.set('Content-Type', 'application/xml');
+    res.status(200).send(req.rawBody);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+
+});
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 3000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
