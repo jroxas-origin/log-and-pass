c17c4552719dc17931158dd2559554a98e1f8200
Justine Roxas <roxas.justine.zs@gmail.com>
2022-06-21 07:23:16 -0700

    cleaned-up code

--- index.js
+++ index.js
@@ -6,22 +6,14 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  //app.get('/', verifyCognitoToken, verifyIDCSToken, verifyAuthenticated, (req, res) => {
   res.send("Hello from server");
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.status(200).send("Hello from server");
+  res.status(200).send({ message: 'Hello from server!', body: req.body });
 });
 
-
-app.post("/get-appointments", (req, res) => {
-  console.log(req.body);
-  //app.get('/', verifyCognitoToken, verifyIDCSToken, verifyAuthenticated, (req, res) => {
-  res.send("Hello from server");
-});
-
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
